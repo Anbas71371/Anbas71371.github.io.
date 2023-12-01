ba2ec4dfebbd003ee5b17a201af2a4abea33cebd
Anbas <andisulo434@gmail.com>
2023-12-01 22:44:47 +0800

    portofolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+  
+    },
+    extend: {
+      colors: {  // Perubahan 'color' menjadi 'colors'
+        primary: '#2563eb',
+        secondary :'#9ca3af',
+        dark : '#27272a'
+      },
+    },
+   
+  },
+  plugins: [],
+};
